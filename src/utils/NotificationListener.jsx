c443e497554c9f35f1e086e145f51b698e52d4d6
RudeusGrayrat <miguelnc2502@gmail.com>
2025-12-10 15:35:36 -0500

    sin notificaciones

--- src/utils/NotificationListener.jsx
+++ src/utils/NotificationListener.jsx
@@ -2,20 +2,20 @@ import { useEffect } from "react";
 import socket from "../api/socket";
 
 export default function NotificationListener({ userId, onNewNotification }) {
-  useEffect(() => {
-    if (!userId) return;
+  // useEffect(() => {
+  //   if (!userId) return;
 
-    socket.emit("join", userId); // Le dice al backend quién es
+  //   socket.emit("join", userId); // Le dice al backend quién es
 
-    socket.on("new_notification", (data) => {
-      console.log("📩 Nueva notificación:", data);
-      onNewNotification(data); // Notifica al componente padre
-    });
+  //   socket.on("new_notification", (data) => {
+  //     console.log("📩 Nueva notificación:", data);
+  //     onNewNotification(data); // Notifica al componente padre
+  //   });
 
-    return () => {
-      socket.off("new_notification");
-    };
-  }, [userId]);
+  //   return () => {
+  //     socket.off("new_notification");
+  //   };
+  // }, [userId]);
 
   return null;
 }
