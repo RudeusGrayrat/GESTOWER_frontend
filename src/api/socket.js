ac44f50b71bb6697bb82fc06c239ec9cb7d09fa1
RudeusGrayrat <miguelnc2502@gmail.com>
2025-12-10 15:34:41 -0500

    sin notificaciones

--- src/api/socket.js
+++ src/api/socket.js
@@ -3,22 +3,22 @@ import "react";
 
 // Obtén la URL del servidor de las variables de entorno
 const urlServer = import.meta.env.VITE_SERVER_URL;
+const socket =" io(urlServer, {"
+// // Crear la conexión de socket
+// const socket = io(urlServer, {
+//   withCredentials: true, // Si estás usando cookies o credenciales
+//   transports: ["websocket"], // Preferir WebSocket como transporte
+// });
 
-// Crear la conexión de socket
-const socket = io(urlServer, {
-  withCredentials: true, // Si estás usando cookies o credenciales
-  transports: ["websocket"], // Preferir WebSocket como transporte
-});
+// // // Emitir el evento de "join" para registrar al usuario en la sala correspondiente
+// // if (user) {
+// //   socket.emit("join", user._id); // Se le dice al servidor quién es el usuario
+// // }
 
-// // Emitir el evento de "join" para registrar al usuario en la sala correspondiente
-// if (user) {
-//   socket.emit("join", user._id); // Se le dice al servidor quién es el usuario
-// }
-
-// Escuchar mensajes o notificaciones desde el servidor
-socket.on("notification", (data) => {
-  console.log("Notificación recibida:", data);
-  // Aquí puedes agregar lógica para manejar las notificaciones
-});
+// // Escuchar mensajes o notificaciones desde el servidor
+// socket.on("notification", (data) => {
+//   console.log("Notificación recibida:", data);
+//   // Aquí puedes agregar lógica para manejar las notificaciones
+// });
 
 export default socket;
